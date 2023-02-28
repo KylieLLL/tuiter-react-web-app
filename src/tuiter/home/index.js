15789c33b31b85396ff4da79d0dc57697e4c3ae5
KylieLLL <xu.yuanh@northeastern.edu>
2023-02-27 20:40:30 -0800

    a7 Redux sp23

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import PostList from "../post-list";
+import TuitsList from "../tuits/index";
+import WhatsHappening from "./whats-happening";
 
 const HomeComponent = () => {
     return (
         <div>
             <div className="">
-                <PostList/>
+                <h4>Home</h4>
+                <WhatsHappening/>
+                <TuitsList/>
             </div>
         </div>
     );
